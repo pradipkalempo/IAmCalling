b2d742d3974cc294c061584e2d7a9af7d59bd440
Pradip Kale <kalepradip2003@gmail.com>
2026-03-13 17:22:52 +0530

    Update universal topbar header to 'DebateME'

--- iamcalling/public/js/universal-topbar.js
+++ iamcalling/public/js/universal-topbar.js
@@ -80,7 +80,7 @@
             topbar.innerHTML =
                 '<div class="topbar-container">' +
                 '<div class="topbar-left">' +
-                '<a href="/" class="topbar-logo"><i class="fas fa-phone"></i><span>Debate Me</span></a>' +
+                '<a href="/" class="topbar-logo"><i class="fas fa-phone"></i><span>DebateME</span></a>' +
                 '</div>' +
                 '<div class="topbar-center">' +
                 '<nav class="topbar-nav" id="topbar-nav">' +
